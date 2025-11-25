08b2b7badbdc5840ae69215dfb688bb57cd6886f
Bunny-Ty <cmxuanhoa@gmail.com>
2025-11-25 21:26:16 +0700

    update thông tin giai thich code

--- src/main/java/org/example/session1/FirstJavaProgram.java
+++ src/main/java/org/example/session1/FirstJavaProgram.java
@@ -5,3 +5,11 @@ public class FirstJavaProgram {
         System.out.println("This is my first program in java");
     } //End of main
 } //End of First Java Program
+
+// public: phương thức có thể được gọi từ bất kỳ nơi nào.
+// static: phương thức thuộc về lớp, không cần tạo đối tượng để chạy.
+// void: phương thức không trả về giá trị.
+// main: tên phương thức đặc biệt mà JVM tìm để chạy chương trình.
+// String[] args: tham số truyền vào chương trình (nếu chạy bằng dòng lệnh).
+//System.out.println() là lệnh in ra màn hình console.
+
