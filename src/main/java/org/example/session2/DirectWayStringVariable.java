8a8c39ed637f7f9a948a996ca95ebd134378d271
Bunny-Ty <cmxuanhoa@gmail.com>
2025-11-29 22:54:36 +0700

    Direct way

---
+++ src/main/java/org/example/session2/DirectWayStringVariable.java
@@ -0,0 +1,8 @@
+package org.example.session2;
+
+public class DirectWayStringVariable {
+    public void directWayStringVariable(){
+        String greet = "Good Morning";
+        System.out.println(greet);
+    }
+}
