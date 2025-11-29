81a8e54d0ca44949c82e244aced8cf39a1fc33b8
Bunny-Ty <cmxuanhoa@gmail.com>
2025-11-29 23:07:31 +0700

    String Length

---
+++ src/main/java/org/example/session2/MyClass.java
@@ -0,0 +1,11 @@
+package org.example.session2;
+
+//String Length - Do dai chuoi
+// Returns the number of characters contained in the string object - Tra ve so ky tu co trong doi tuong chuoi
+
+public class MyClass {
+    public static void main(String[] args){
+        String txt = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        System.out.println("The lenghth of the txt string is: " + txt.length());
+    }
+}
