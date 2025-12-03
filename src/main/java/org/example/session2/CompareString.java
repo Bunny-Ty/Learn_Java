3f3aba8914c0346849967d92882d08cd109b5e56
Bunny-Ty <cmxuanhoa@gmail.com>
2025-12-03 22:19:26 +0700

    Compare String

---
+++ src/main/java/org/example/session2/CompareString.java
@@ -0,0 +1,10 @@
+package org.example.session2;
+
+public class CompareString {
+    public void compareString(){
+        String s1 = new String("HELLO");
+        String s2 = new String("HELLO");
+        System.out.println(s1 == s2);
+        System.out.println(s1.equals(s2));
+    }
+}
