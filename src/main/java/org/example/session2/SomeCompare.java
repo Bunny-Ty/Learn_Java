19ac78f0e2e5471e65e00166d536ce61b3571a10
Bunny-Ty <cmxuanhoa@gmail.com>
2025-12-03 22:23:05 +0700

    Some Compare

---
+++ src/main/java/org/example/session2/SomeCompare.java
@@ -0,0 +1,14 @@
+package org.example.session2;
+
+public class SomeCompare {
+    public void someCompare(){
+        // Compare integer
+        System.out.println(100 == 100);
+        // Compare char
+        System.out.println('a' == 'a');
+        // Compare char & float
+        System.out.println('a' == 97.1f);
+        // Compare boolean
+        System.out.println(true == true);
+    }
+}
