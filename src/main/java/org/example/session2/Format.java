52a959d7e392f50d51c697a4e44154ce4d7809a7
Bunny-Ty <cmxuanhoa@gmail.com>
2025-12-03 22:16:23 +0700

    format()

---
+++ src/main/java/org/example/session2/Format.java
@@ -0,0 +1,8 @@
+package org.example.session2;
+
+public class Format {
+    public void stringFormat(){
+        String value = "Testing VN";
+        String formatted = String.format("Welcome to %s!", value);
+    }
+}
