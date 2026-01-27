c8a1100fd9fb3d6786826056a865cb424804376f
Bunny-Ty <cmxuanhoa@gmail.com>
2026-01-27 21:27:39 +0700

    Add example code for If statement

---
+++ src/main/java/org/example/session3/IfStatement.java
@@ -0,0 +1,23 @@
+package org.example.session3;
+
+public class IfStatement {
+
+    public static void main (String[] args){
+        int x = 10; // khai bao bien x va gan gia tri 10
+        int y = 20; // khai bao bien y va gan gia tri 20
+
+        if ( x > y) {
+            System.out.println("x lon hon y");
+        }
+
+        if ( x < y) {
+            System.out.println("x nho hon y");
+        }
+
+        if ( x == y) {
+            System.out.println(" x bang y");
+        }
+
+        System.out.println("Chuong trinh ket thuc");
+    }
+}
