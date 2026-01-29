752fc023be4f23d5b27ae2a9a1d9fd92291d1345
Bunny-Ty <cmxuanhoa@gmail.com>
2026-01-29 21:48:03 +0700

    Add example code for If Else Statemnent

---
+++ src/main/java/org/example/session3/IfElseStatement.java
@@ -0,0 +1,24 @@
+package org.example.session3;
+
+public class IfElseStatement {
+
+    // Ham main - diem bat dau cua chuong trinh java
+    public static void main (String[] args){
+
+        // Khai bao bien time va gan gia tri la 20
+        int time = 20;
+
+        // Kiem tra dieu kien: neu time nho hon 18
+        if (time < 18) {
+
+            // Neu dieu kien dung, in ra "Good day."
+            System.out.println("Good day.");
+
+             }  else {
+
+                // Neu dieu kien sai, in ra "Good evening."
+                    System.out.println("Good evening.");
+        }
+        // Chuong trinh ket thuc sau khi chay xong if-else
+    }
+}
